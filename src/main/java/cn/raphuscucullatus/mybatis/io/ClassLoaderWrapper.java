a64b0d2eb1968e78e00bd442bd3fc834a5ac5e97
raphuscucullatus <993275475@qq.com>
2021-09-02 16:28:19 +0800

    Initial commit

---
+++ src/main/java/cn/raphuscucullatus/mybatis/io/ClassLoaderWrapper.java
@@ -0,0 +1,9 @@
+package cn.raphuscucullatus.mybatis.io;
+
+/**
+ * @author raphus cucullatus
+ * @version 2021/9/114:34
+ * @since JDK8
+ */
+public class ClassLoaderWrapper {
+}
---
+++ src/main/java/cn/raphuscucullatus/mybatis/io/Resources.java
@@ -0,0 +1,14 @@
+package cn.raphuscucullatus.mybatis.io;
+
+import java.io.InputStream;
+
+/**
+ * @author raphus cucullatus
+ * @version 2021/9/114:24
+ * @since JDK8
+ */
+public class Resources {
+    public static InputStream getResourceAsStream(String resource){
+
+    }
+}
